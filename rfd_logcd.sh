205300fd0665b5bb16d8904a8466e9a576a8141a
amoore2600 <visual.effects.artist@gmail.com>
2019-07-27 13:19:38 -0400

    Update rfd_logcd.sh

--- rfd_logcd.sh
+++ rfd_logcd.sh
@@ -20,6 +20,7 @@ do
 	do
 		[ -f "$dstd/$logfile" ] || busybox sleep 5 && busybox cp "$srcd/$logfile" "$dstd/${eromdump}"  
 	done
+	                   # copy SRAM remove first 24 bits and decompress SRAM
                             eromdump=`busybox ls /mnt/external_sd/RetroFreak/Games/`
                             fnesramdump="`busybox find "/mnt/external_sd/RetroFreak/Saves/" -name *.sav | busybox awk -F "/" '{print$8}'`"
                             if [ ! -z "${fnesramdump}" ]
